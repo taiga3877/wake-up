d24160ca29656b79af74df727c6fd85ad8898ace
taiga3877 <taiga3877@gmail.com>
2025-02-18 15:19:48 +0500

    add

--- src/Pages/Error404/Error404.jsx
+++ src/Pages/Error404/Error404.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import SplitType from "split-type";
+import { NavLink } from "react-router-dom";
 
 
 export default function NotFoundPage() {
@@ -33,8 +34,13 @@ export default function NotFoundPage() {
 
   return (
     <div className="flex items-center justify-center h-screen bg-black text-white">
+      
       <div className="text-center relative">
-        <p ref={textRef} className="text-2xl">404, page not found.</p>
+        <p ref={textRef} className="text-2xl"><NavLink to={'/'}>404, page not found.
+       
+        Go At Home</NavLink> </p>
+        
+        
         <span ref={handleRef} className="absolute bg-yellow-400 w-4 h-8 top-0 left-0 mt-1"></span>
         <svg
           ref={replayRef}
@@ -44,6 +50,7 @@ export default function NotFoundPage() {
           viewBox="0 0 279.9 297.3"
           className="w-6 absolute right-0 bottom-0 cursor-pointer transition-transform duration-300 hover:rotate-12 hover:fill-gray-400"
         >
+          
           <path d="M269.4,162.6c-2.7,66.5-55.6,120.1-121.8,123.9c-77,4.4-141.3-60-136.8-136.9C14.7,81.7,71,27.8,140,27.8 c1.8,0,3.5,0,5.3,0.1c0.3,0,0.5,0.2,0.5,0.5v15c0,1.5,1.6,2.4,2.9,1.7l35.9-20.7c1.3-0.7,1.3-2.6,0-3.3L148.6,0.3 c-1.3-0.7-2.9,0.2-2.9,1.7v15c0,0.3-0.2,0.5-0.5,0.5c-1.7-0.1-3.5-0.1-5.2-0.1C63.3,17.3,1,78.9,0,155.4 C-1,233.8,63.4,298.3,141.9,297.3c74.6-1,135.1-60.2,138-134.3c0.1-3-2.3-5.4-5.3-5.4l0,0C271.8,157.6,269.5,159.8,269.4,162.6z" />
         </svg>
       </div>
